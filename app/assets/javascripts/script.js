e6c93d0202f073409f556d119c9b826724dad027
Hee Jae Chae <poongnewga@gmail.com>
2017-05-25 23:53:13 +0900

    Add nav-bar toggle script

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,6 +2,16 @@
 $(document).ready(function(){
   $("#loading").css({"position": "fixed", "width": "100vw", "height": "100vh", "background-color": "white", "z-index": "99"});
 
+// Header navigation toggle
+  $(".h-link-c label").click(function(){
+    var clicked = $(this).index();
+    $("input[id*=tog]").each(function(index){
+      if (clicked != index) {
+        $(this).attr('checked', false);
+      }
+    })
+  })
+
 });
 
 $(window).load(function(){
