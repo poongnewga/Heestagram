eae33df5b55f27d10e1a4532a20efefd68cbed99
Hee Jae Chae <poongnewga@gmail.com>
2017-05-26 11:24:21 +0900

    Add Sign-up Error text

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts, dependent: :destroy
 
-  validates :email, presence: true, uniqueness: true
-  validates :name, presence: true, uniqueness: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
 end
